4dac141d5621dd7c686a17c7da4ffd2355ebe10a
Bobgy <gongyuan94@gmail.com>
2015-06-27 14:41:30 +0800

    Moved index.js to /bin/www.js, left an index.js to be required

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// the game server that supports multiple games
 var Server = require('./lib/server');
-var server = new Server(io);
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'js')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-http.listen(3000, function() {
-  console.log('listening on *:3000')
-});
-
-server.listen();
+module.exports = Server;
